dd9cc53a74e23e705920039ae55e39943972b052
Ricardo Santos <sricardosdsouza@yahoo.com>
2021-10-07 15:59:02 -0300

    Atualizando formularios
    criando paginas de cursos e aulas

---
+++ cursos/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from .models import Cursos, Aulas
+  
+def home(request):
+    if request.session.get('usuario'):
+        cursos = Cursos.objects.all()
+        request_usuario = request.session.get('usuario')
+        return render(request, 'home.html', {'cursos': cursos, 'request_usuario': request_usuario})
+    else:
+        return redirect('/auth/login/?status=2')
+    
+def curso(request, id):
+    v_curso = Cursos.objects.get(id = id)
+    aulas = Aulas.objects.filter(curso = v_curso)
+    return render(request, 'curso.html', {'aulas': aulas})
+
+def aula(request, id):
+    if request.session.get('usuario'):
+        aula = Aulas.objects.get(id = id)
+        return render(request, 'aula.html', {'aula': aula})
+    else:
+        return redirect('/auth/login/?status=2')
